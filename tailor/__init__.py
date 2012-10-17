2c9571dd744f16920e79fdb28ab4e0c96281e950
Crispin Wellington <retrogradeorbit@gmail.com>
2012-10-17 15:18:24 +0800

    added module base info

---
+++ tailor/__init__.py
@@ -0,0 +1,9 @@
+__title__ = 'tailor'
+__version__ = '0.1'
+__author__ = 'Crispin Wellington'
+__copyright__ = 'Copyright 2012 Crispin Wellington'
+
+from . import revisions
+from . import versions
+from . import deploy
+from . import distro
