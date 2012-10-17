6295dcbb8ded49210ab8d66491b721fc7112a2ca
Crispin Wellington <retrogradeorbit@gmail.com>
2012-10-17 15:21:41 +0800

    fixing build

--- tailor/__init__.py
+++ tailor/__init__.py
@@ -3,7 +3,3 @@ __version__ = '0.1'
 __author__ = 'Crispin Wellington'
 __copyright__ = 'Copyright 2012 Crispin Wellington'
 
-from . import revisions
-from . import versions
-from . import deploy
-from . import distro
