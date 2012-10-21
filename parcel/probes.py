b2f395558056eb6a0a4a46e4dad11ed169894f22
Crispin Wellington <retrogradeorbit@gmail.com>
2012-10-21 17:16:38 +0800

    deb probes for .deb control files, package tree. also nice colour output

--- parcel/probes.py
+++ parcel/probes.py
@@ -3,17 +3,40 @@
 #
 
 from fabric.api import put, cd, run
+from fabric.colors import green
 
 from .distro import debian
+from .tools import read_contents_from_remote, quiet_run
 
 def deb_ls(deb):
     base_dir, src_dir, build_dir = debian._setup()
     put(deb,build_dir+"/"+deb)
     with cd(build_dir):
-        run("dpkg --contents '%s'"%deb)
+        print green(quiet_run("dpkg --contents '%s'"%deb))
                 
 def deb_install(deb):
     base_dir, src_dir, build_dir = debian._setup()
     put(deb,build_dir+"/"+deb)
     with cd(build_dir):
-        run("dpkg --install '%s'"%deb)
+        print green(quiet_run("dpkg --install '%s'"%deb))
+
+def deb_control(deb):
+    base_dir, src_dir, build_dir = debian._setup()
+    put(deb,build_dir+"/"+deb)
+    with cd(build_dir):
+        run("dpkg --control '%s'"%deb)
+        files = run("find DEBIAN -type f -print").strip().splitlines()
+        for fname in files:
+            data = read_contents_from_remote(fname)
+            print green(fname,True)
+            print green("="*len(fname),True)
+            print green(data)
+
+def deb_tree(deb):
+    base_dir, src_dir, build_dir = debian._setup()
+    put(deb,build_dir+"/"+deb)
+    with cd(build_dir):
+        run("dpkg --extract '%s' root"%deb)
+        with cd('root'):
+            print green(quiet_run("tree"))
+        
