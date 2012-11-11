9fb695859d50365a3d4de7e118f0e07066c26930
Andrew Macgregor <andrew@ccg.murdoch.edu.au>
2012-11-11 11:42:24 +0800

    quieting simple http server output so test output is cleaner

--- tests/mixins.py
+++ tests/mixins.py
@@ -7,6 +7,7 @@ import socket, errno
 class ThreadedTCPServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
     pass
 
+
 class WebServerMixin(object):
     """Webserver test cases."""
     def startWebServer(self,port=8000):
@@ -24,6 +25,12 @@ class WebServerMixin(object):
         self.ip, self.port = self.server.server_address
         
         self.webroot = os.path.join(os.path.dirname(__file__),"data")           # serve out the data directory
+
+        # quiet down the messages from SimpleHTTPRequestHandler, remove these lines for more output
+        def log_message(self, format, *args):
+            return
+        self.server.RequestHandlerClass.log_message = log_message
+
         
         # Start a thread with the server
         def sthread():
