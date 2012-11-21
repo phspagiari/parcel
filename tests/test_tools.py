1d4e99317d046dd2847a2101a21c4386b5f3fad1
Andrew Macgregor <andrew@ccg.murdoch.edu.au>
2012-11-21 20:12:41 +0800

    full coverage of rsync

--- tests/test_tools.py
+++ tests/test_tools.py
@@ -2,7 +2,7 @@ import unittest2 as unittest
 from fabric.api import run
 import tempfile
 
-from mock import patch
+from mock import patch, MagicMock
 
 from mixins import WebServerMixin
 from parcel.tools import dl, read_contents_from_remote, quiet_run, rsync
@@ -11,6 +11,8 @@ from parcel_mocks import mock_get, local
 # add mocks to this list if they should have reset called on them after tests
 mocks_to_reset = []
 
+rsync_run = MagicMock(name="rsync_run")
+rsync_local = MagicMock(name="rsync_local")
 
 def tempname():
     return tempfile.mkstemp()[1]
@@ -79,12 +81,30 @@ class ToolsTestSuite(unittest.TestCase, WebServerMixin):
         self.assertTrue('tip.tar.gz' in data)
 
 
-    @patch('parcel.tools.run', local)
-
     # patch local so rsync command not run, check correct command is called
-    def _test_rsync(self):
+    @patch.multiple('parcel.tools', run=rsync_run, local=rsync_local)
+    def test_rsync(self):
         test_file = os.path.join(os.path.dirname(__file__),"data", "tip.tar.gz")
-        #make a dir - rsync into it
-        # test
-        # remove
+        rsync_local.return_value = "This is output\n from test_rsync local mock.\n\n This is the fourth line.\n"
         rsync(test_file, 'test.tar.gz')
+
+        # check rsync was called
+        self.assertTrue('rsync -av' in rsync_local.call_args[0][0])
+
+        # with colors off
+        rsync(test_file, 'test.tar.gz', color_files=False)
+        
+        # with rsync_ignore, not real rsync file, just has to exist in this case
+        # as we are not really calling rsync
+        ignore_file = os.path.join(os.path.dirname(__file__),"data", "hello.py")
+        rsync(test_file, 'test.tar.gz', rsync_ignore=ignore_file)
+        self.assertTrue('--exclude-from={}'.format(ignore_file) in rsync_local.call_args[0][0])
+
+        # with rsync_ignore but a non-existent file
+        rsync(test_file, 'test.tar.gz', rsync_ignore='rsync_ignore')
+        self.assertTrue('--exclude-from' not in rsync_local.call_args[0][0])
+
+        # call with a list
+        rsync([test_file, 'another_test_file'], 'test_files/')
+        command = "rsync -av '{0}' '{1}'".format(test_file, 'another_test_file')
+        self.assertTrue(command in rsync_local.call_args[0][0])
