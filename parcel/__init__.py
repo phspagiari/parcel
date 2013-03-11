39ee33dd65db52575e3aa4aed2716e5a9b4259ec
Crispin Wellington <retrogradeorbit@gmail.com>
2013-03-11 18:26:24 +0800

    copyright year extended
    --HG--
    branch : develop

--- parcel/__init__.py
+++ parcel/__init__.py
@@ -1,5 +1,5 @@
 __title__ = 'parcel'
 __version__ = '0.1.1'
 __author__ = 'Crispin Wellington'
-__copyright__ = 'Copyright 2012 Crispin Wellington'
+__copyright__ = 'Copyright 2012-2013 Crispin Wellington'
 
